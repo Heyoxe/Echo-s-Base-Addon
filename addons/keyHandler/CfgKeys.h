06696679a548a3a2bc8442377e115e20fee351b3
Heyoxe <heyoxe@gmail.com>
2019-05-13 14:25:56 +0200

    Base Config

---
+++ addons/keyHandler/CfgKeys.h
@@ -0,0 +1,26 @@
+class EBA_keysConfig_M {
+    class ESE_keysConfig {
+        name = "Echo's Sandbox Everywhere";
+        tooltip = "Echo's mod bla bla bla";
+        class actions {
+            class openMenu {
+                name = "Open Menu";
+                tooltip = "Open ESE's Menu";
+                script = "";
+                defaultKeys[] = {{"RIGHT CTRL", "E"}, {"H", "HOLD"}};
+            };
+            class openMenu {
+                name = "Open Menu";
+                tooltip = "Open ESE's Menu";
+                script = "";
+                defaultKeys[] = {{"RIGHT CTRL", "RIGHT ALT", "R"}, {"R", "E"}};
+            };
+            class openMenu {
+                name = "Open Menu 3";
+                tooltip = "Open ESE's Menu";
+                script = "";
+                defaultKeys[] = {{"E"}, {"H", "DOUBLE"}};
+            };
+        };
+    };
+};
---
+++ mod.cpp
@@ -0,0 +1,8 @@
+name = "Echo's Base Addon";
+author = "Echo (Heyoxe)";
+logoSmall = ""; //TODO: REMPLACE
+logo = ""; //TODO: REMPLACE
+logoOver = ""; //TODO: REMPLACE
+action = "https://steamcommunity.com/id/Heyoxe/";
+tooltipOwned = "Echo's Base Addon Loaded!";
+overview = "Common functionality for Echo's mods"; //TODO: Better Description
